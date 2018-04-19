5d1cd0e78a2068e8a8dbece6c9d500001bdd93c4
andreasvirkus <andreasvirkus@gmail.com>
2018-04-19 18:29:57 +0300

    Add background sync example base

--- README.md
+++ README.md
@@ -5,12 +5,28 @@ ServiceWorkers.
 
 Slides can be found in `/index.html` and they're built with [reveal.js](https://github.com/hakimel/reveal.js).
 
-Various (really?) examples can be found in `/examples` (and hopefully will be updated in the future with more
+Various examples can be found in `/examples` (and hopefully will be updated in the future with more
 complex additions).
 
 Feel free to use and/or modify this to your liking.
 
+To present:
+
+```
+⚡ git clone https://github.com/andreasvirkus/pwa-workshop.git
+⚡ cd pwa-workshop/slides
+⚡ npm i && npm start
+```
+
+See a more detailed usage guide in [/slides](slides/README.md)
+
+Great PWA/SW resources:
+- [deanhume/pwa-tips-tricks](https://github.com/deanhume/pwa-tips-tricks)
+- the [spec](https://github.com/w3c/ServiceWorker)
+- Webpack [plugin](https://www.npmjs.com/package/sw-precache-webpack-plugin)
+
 ## TODO:
-- Example & workshop using BackgroundSync
+- ~Example of offline support~
+- ~Example & workshop using BackgroundSync~
 - Example & workshop using push notifications
 - Example & workshop using scoped workers
---
+++ examples/background-sync/js/script.js
@@ -0,0 +1,53 @@
+(function() {
+  const allNavItems = $('.main-nav li')
+  const allTabs = $('.tab')
+  const reqButton = $('#request-button')
+
+  allNavItems.on('click', function(e) {
+    e.preventDefault()
+
+    allNavItems.removeClass('active')
+    allTabs.removeClass('active')
+
+    $(this).addClass("active");
+
+    const selector = $(this).find("a").attr("href");
+    $(selector).addClass("active");
+  })
+
+  function isOnline () {
+    const connectionStatus = $('#connection-status');
+    const status = navigator.onLine
+     ? '🍏 You are currently online!'
+     : '🍎 You are currently offline. Any requests made will be queued and synced as soon as you are connected again.'
+
+    connectionStatus.text(status)
+  }
+
+  window.addEventListener('online', isOnline);
+  window.addEventListener('offline', isOnline);
+  isOnline();
+
+  if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.')
+    navigator.serviceWorker.register('/worker.js')
+      // .then(reg => {
+      //   console.log('CLIENT: service worker registration complete:', reg)
+      //   return navigator.serviceWorker.ready
+      // })
+      .then(reg => navigator.serviceWorker.ready)
+      .then(registration => { // register sync
+        console.log('CLIENT: registering sync');
+        reqButton.on('click', () => {
+          registration.sync.register('image-fetch').then(() => {
+              console.log('CLIENT: sync registered')
+          })
+        })
+      })
+      .catch(err => console.error('CLIENT: service worker registration failure:', err))
+  } else {
+    reqButton.on('click', () => {
+      console.log('CLIENT: Fallback to fetch the image as usual')
+    })
+  }
+})()
